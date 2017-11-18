029ee630e313b1408bb2a0227210876ebddc9748
Your Name <you@example.com>
2017-11-18 14:00:44 -0800

    Changed the HTML for the site.

---
+++ hello.py
@@ -0,0 +1,17 @@
+print("hello")
+print("hi alex")
+x = "hi alex"
+print(x)
+x = "there are two alex"
+print(x)
+x = "django girls project"
+print(x)
+x = ["tomato","potato","ice cream"]
+print(x)
+for food in x:
+    print(food)
+x = [1,2,3,4,5]
+sum = 0
+for num in x:
+    sum += num
+print(sum)
